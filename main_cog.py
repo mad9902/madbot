4750eca900d3278c65f7ef2a6880cd9c09fddb4f
muhammad9902 <anis.assegaf2002@gmail.com>
2025-06-15 00:17:07 +0700

    update command ping

--- main_cog.py
+++ main_cog.py
@@ -3,6 +3,7 @@ import random
 import asyncio
 
 from discord.ext import commands
+from discord import app_commands
 
 class main_cog(commands.Cog):
     def __init__(self, bot):
@@ -19,7 +20,16 @@ class main_cog(commands.Cog):
         for guild in self.bot.guilds:
             for channel in guild.text_channels:
                 self.text_channel_list.append(channel)
-        
+                
+    @app_commands.command(name="ping", description="Cek latensi bot.")
+    async def slash_ping(self, interaction: discord.Interaction):
+        latency = round(self.bot.latency * 1000)
+        await interaction.response.send_message(f"[SLASH] Pong! 🏓 Latensi: {latency}ms")
+
+    @commands.command(name="ping", help="Cek latensi bot.")
+    async def prefix_ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.send(f"Pong! 🏓 Latensi: {latency}ms")
 
     @commands.command(name="help", aliases=["h"], help="Displays all the available commands")
     async def help(self, ctx):
